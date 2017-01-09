389fae43db36ce85fb800e602fce5bd529224635
BearWarrior <thgallais@gmail.com>
2017-01-09 15:40:52 +0100

    getTuples in inputOutput

--- src/tse/pagerank/io/InputOutput.java
+++ src/tse/pagerank/io/InputOutput.java
@@ -1,9 +1,80 @@
 package tse.pagerank.io;
 
-public class InputOutput 
- {
-	
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.zip.GZIPInputStream;
 
+public class InputOutput 
+{
+
+
+	public static void readSGZ(File file)
+	{
+
+		final String prefixInsert = "INSERT INTO `category` VALUES ";
+		final String suffixInsert = ";";
+
+		try 
+		{
+			BufferedReader in = new BufferedReader(new InputStreamReader(new GZIPInputStream(new FileInputStream(file)), "UTF-8"));
+
+			String line = "";
+			long tpsDebut = System.currentTimeMillis();
+			while((line = in.readLine()) != null)
+			{
+				if(line.startsWith(prefixInsert) && line.endsWith(suffixInsert))
+				{
+					String valuesText = line.substring(prefixInsert.length(), line.length() - suffixInsert.length()); //(...),(...),(...)
+					getTuples(line);
+				}
+			}
+
+			in.close();
+			System.out.println(System.currentTimeMillis() - tpsDebut);
+
+		} catch (IOException e) 
+		{
+			// TODO Auto-generated catch block
+			// Known error which does not interfer with the reading Unexpected end of ZLIB input stream
+			// http://bugs.java.com/bugdatabase/view_bug.do;jsessionid=53ede10dc8803210b03577eac43?bug_id=6519463 
+			//e.printStackTrace();
+		}	
+	}
+
+	public static LinkedList<String> getTuples(String line)
+	{
+		LinkedList<String> tuples = new LinkedList<String>();
+
+		int indexD = 0;
+		int indexF = 0;
+		boolean findStart = true;
+		while (indexD >= 0)
+		{
+			if(findStart)
+			{
+				indexD = line.indexOf('(', indexD + 1);
+				findStart = false;
+			}
+			else
+			{
+				indexF = line.indexOf(')', indexF + 1);
+				findStart = true;
+				
+				tuples.add(line.substring(indexD,  indexF));
+
+				//System.out.println(line.substring(indexD+1,  indexF));
+			}
+		}
+
+		return tuples;
+	}
 	
-	
+	/*public static LinkedList<LinkedList<String>> getColumns(LinkedList<String> tuples)
+	{
+		
+	}*/
 }
--- src/tse/pagerank/main/Main.java
+++ src/tse/pagerank/main/Main.java
@@ -1,7 +1,15 @@
 package tse.pagerank.main;
 
+import java.io.File;
+
+import tse.pagerank.io.InputOutput;
+
 public class Main {
- public static void main(String[] args) {
+ public static void main(String[] args) 
+ {
+	 File file = new File("simplewiki-20161020-category.sql.gz");
+	 
+	 InputOutput.readSGZ(file);
 	System.out.println("hello world!");
  }
 }
